1845f3414f64a4d2e37fa3eb69d11a262c405726
Shwetalbhargav <hridaan.purav@gmail.com>
2025-05-17 19:31:36 +0530

    improvised env file

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-// api.js
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://code-editor-backend-pync.onrender.com";
-
+const API_BASE_URL = process.env.VITE_API_BASE_URL || "https://code-editor-backend-pync.onrender.com";
 
 
 // POST: Run code
