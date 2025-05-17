6e82e65cfc7cb39fa0e173192e4c3ec27bf2a992
Shwetalbhargav <hridaan.purav@gmail.com>
2025-05-17 19:28:21 +0530

    improvised env file

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "https://code-editor-backend-pync.onrender.com";
+// api.js
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "https://code-editor-backend-pync.onrender.com";
+
 
 
 // POST: Run code
